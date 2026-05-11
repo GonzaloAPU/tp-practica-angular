7db5afdd8a464fb95eede038d5461b99eff40242
AldoDante <dane.acuario79@gmail.com>
2026-05-11 11:30:24 -0300

    agreegado de Angular datatables

--- src/app/components/parte2/parte2.ts
+++ src/app/components/parte2/parte2.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Inscripcion } from '../../services/inscripcion';
 import { Alumno, categoriaAlumno } from '../../models/alumno';
+import { DataTablesModule } from 'angular-datatables';
 
 @Component({
   selector: 'app-parte2',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, DataTablesModule],
   templateUrl: './parte2.html',
   styleUrl: './parte2.css',
 })
@@ -16,8 +17,19 @@ export class Parte2 {
   inscripciones: Array<Alumno> = [];
   categoriaAlumno = categoriaAlumno;
   nuevoAlumno: Alumno = new Alumno('', 0, categoriaAlumno.Estudiante, '', '', '');
+  dtOptions: any = {};
 
-  constructor(private inscripcionService: Inscripcion) {
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers', // Muestra todos los numeritos de paginación
+      pageLength: 5, // Muestra de a 5 filas
+      language: {
+        // Esto traduce mágicamente el buscador y los botones al español
+        url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json' 
+      }}}
+
+
+  constructor(private inscripcionService: Inscripcion, private tabla: DataTablesModule) {
     this.inscripciones = this.inscripcionService.getInscripciones();
   }
 
