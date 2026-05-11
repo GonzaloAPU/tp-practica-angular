3e3b116bef034085bdea0a857ab4c4f39387ac14
Sonia Gareca <gsoniaemiliana@gmail.com>
2026-05-11 13:15:55 -0300

    Estilos en punto 2

--- src/app/components/punto2/punto2.ts
+++ src/app/components/punto2/punto2.ts
@@ -9,12 +9,12 @@ import {CommonModule} from '@angular/common';
 })
 export class Punto2 {
   productos = [
-    { nombre: 'Camiseta', descripcion: 'Camiseta de algodón con diseño moderno', img: 'producto01.jpg', precio: 19.99 },
-    { nombre: 'Pantalones', descripcion: 'Pantalones de mezclilla cómodos y duraderos', img: 'producto02.jpg', precio: 29.99 },
-    { nombre: 'Zapatos', descripcion: 'Zapatos deportivos para todas las actividades', img: 'producto03.jpg', precio: 39.99 },
-    { nombre: 'Chaqueta', descripcion: 'Chaqueta impermeable para protegerte de la lluvia', img: 'producto04.jpg', precio: 49.99 },
-    { nombre: 'Gorra', descripcion: 'Gorra de béisbol con diseño clásico', img: 'producto05.jpg', precio: 9.99 },
-    { nombre: 'Mochila', descripcion: 'Mochila resistente con múltiples compartimentos', img: 'producto06.jpg', precio: 34.99 },
+    { nombre: 'Camiseta', descripcion: 'Camiseta de algodón con diseño moderno', img: 'img/camiseta.jpg', precio: 19.99 },
+    { nombre: 'Pantalones', descripcion: 'Pantalones de mezclilla cómodos y duraderos', img: 'img/pantalones.jpg', precio: 29.99 },
+    { nombre: 'Zapatos', descripcion: 'Zapatos deportivos para todas las actividades', img: 'img/zapatos.jpg', precio: 39.99 },
+    { nombre: 'Chaqueta', descripcion: 'Chaqueta impermeable para protegerte de la lluvia', img: 'img/chaqueta.jpg', precio: 49.99 },
+    { nombre: 'Gorra', descripcion: 'Gorra de béisbol con diseño clásico', img: 'img/gorra.jpg', precio: 9.99 },
+    { nombre: 'Mochila', descripcion: 'Mochila resistente con múltiples compartimentos', img: 'img/mochila.jpg', precio: 34.99 },
   ]
 
   carrito: any[] = [];
