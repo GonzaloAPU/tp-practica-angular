077e6fb6894ac132cf3bd5f9c5224179fc50c4fe
GonzaloAPU <gonzaloc1704@gmail.com>
2026-05-19 13:49:38 -0300

    validaciones en parte2.html

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
-import { Punto1 } from './components/punto1/punto1';
-import { Punto2 } from './components/punto2/punto2';
-import { Punto3 } from './components/punto3/punto3';
-import { Parte2 } from './components/parte2/parte2';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Punto1, Punto2, Punto3, Parte2],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
